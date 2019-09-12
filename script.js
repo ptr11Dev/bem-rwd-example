14ea1f37963cb822f55e30a4d8c8051b2ba4bf01
id github: ptr11Dev <piotrkaczmarek.dev@gmail.com>
2019-09-12 13:09:27 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,76 @@
+// text typing
+/* const cursor = document.querySelector(".header__subtitle-cursor");
+
+const typingSpeed = 200;
+cursorOn = 1;
+
+setCursor = () => {
+  if (cursorOn) {
+    cursor.textContent = "|";
+    cursorOn = 0;
+  } else {
+    cursor.textContent = "";
+    cursorOn = 1;
+  }
+};
+
+setInterval(setCursor, typingSpeed * 1.4);
+
+const subtitleText = document.querySelector(".header__subtitle-text");
+const tempSubtitleText = subtitleText.textContent;
+subtitleText.textContent = "";
+
+let letter = 0;
+addLetter = () => {
+  subtitleText.textContent += tempSubtitleText.charAt(letter);
+  letter++;
+};
+typing = () => {
+  setInterval(addLetter, typingSpeed);
+};
+
+setTimeout(() => {
+  typing();
+}, 2000); */
+
+//buttons
+const btns = document.querySelectorAll(".button");
+
+btns.forEach(btn =>
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("button--active");
+  })
+);
+
+btns[0].addEventListener("click", () => {
+  console.log(`As for now - I'm doing nothing.`);
+});
+
+//nav items
+const options = document.querySelectorAll(".navigation__list-item");
+options.forEach(opt =>
+  opt.addEventListener("click", () => {
+    options.forEach(opt => {
+      opt.classList.remove("navigation__list-item--active");
+    });
+    opt.classList.toggle("navigation__list-item--active");
+  })
+);
+
+//articles;
+
+const section = document.querySelector("section");
+
+const append = data => {
+  data.forEach(element => {
+    let article = document.createElement("article");
+    article.setAttribute = element.id;
+    article.innerHTML = `<h1>${element.name}</h1><blockquote>${element.author}</blockquote><p>${element.content}</p>`;
+    section.appendChild(article);
+  });
+};
+
+fetch("articles.json")
+  .then(res => res.json())
+  .then(res => append(res))
+  .catch(err => console.log(err));
